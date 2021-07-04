f4cde709f6f7f5115fed2aa4afc06ce97e2b4306
Alexandre Jales <alexandre.jales@hotmail.com>
2021-07-03 21:50:45 -0300

    Criação do projeto

---
+++ src/main/java/com/curso/escola/Aluno.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curso.escola;
+
+/**
+ *
+ * @author Alexandre Jales
+ */
+public class Aluno {
+    private String cpf;
+    private String nome;
+    private Email email;
+    
+}
---
+++ src/main/java/com/curso/escola/Email.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.curso.escola;
+
+/**
+ *
+ * @author Alexandre Jales
+ */
+class Email {
+    private String endereco;
+
+    public Email(String endereco) {
+        if (endereco == null || 
+                !endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$")) {
+            throw new IllegalArgumentException("E-mail invalido");
+        }
+        this.endereco = endereco;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
